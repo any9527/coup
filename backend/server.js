875fd8965b0c807e17064f296d7344f66fb00677
Andy Yang <ayang@tenna.com>
2020-03-26 18:10:26 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 require("dotenv").config();
+
 const port = process.env.PORT || 4001;
-var app = require("express")();
-var http = require("http");
+const app = require("express")();
+const http = require("http");
 const socketIo = require("socket.io");
+
 const server = http.createServer(app);
 const io = socketIo(server);
 
 const startPlatform = require("./platform");
+
 startPlatform(io);
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-server.listen(port, function() {
+server.listen(port, () => {
   console.log(`listening on *:${port}`);
 });
